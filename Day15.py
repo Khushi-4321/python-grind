35659389000675769517dc63e5cbd88ffad35e3d
Khushi <kagarwal170106@gmail.com>
2026-03-25 00:05:37 +0530

    Day 15 - recursion: recursive sum and max of list

--- Day15.py
+++ Day15.py
@@ -1,3 +1,7 @@
+# Takes a list of numbers
+# Returns the sum of all elements
+# No loops allowed No sum() built-in
+
 def recursive_sum(num):
     if num == []:
         return 0
@@ -5,3 +9,16 @@ def recursive_sum(num):
 
 num = [6,9,4,8]
 print(recursive_sum(num))
+
+
+# recursive_max([3, 7, 2, 9, 1]) → 9
+def recursive_max(num):
+    if num == []:
+        return 0
+    elif num[0] > recursive_max(num[1:]):
+        return num[0]
+    else: 
+        return recursive_max(num[1:]) 
+    
+num = [3, 7, 2, 9, 1]    
+print(recursive_max(num))
