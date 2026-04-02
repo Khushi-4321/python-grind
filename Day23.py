aec4dd84796102af4d96c3dcc54127d68ccb34ad
Khushi <kagarwal170106@gmail.com>
2026-04-02 23:59:52 +0530

    Day 23 - recursion: count vowels WIP

---
+++ Day23.py
@@ -0,0 +1,12 @@
+# Write a recursive function that counts the number of vowels in a string.
+# count_vowels("hello") → 2
+
+def count_vowels(s):
+    if s =="":
+        return []
+    if s[0] in "AEIOUaeiou":
+        return 1+ count_vowels(s[1:])
+    return 1+ count_vowels(s[1:])
+
+s= "Vowelcheck"
+print(count_vowels(s))
