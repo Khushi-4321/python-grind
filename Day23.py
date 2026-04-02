be1ee79cb397fea26897a00ab41786cf87bd79ec
Khushi <kagarwal170106@gmail.com>
2026-04-03 00:04:05 +0530

    Day 23 - recursion: count vowels completed

--- Day23.py
+++ Day23.py
@@ -3,10 +3,10 @@
 
 def count_vowels(s):
     if s =="":
-        return []
+        return 0
     if s[0] in "AEIOUaeiou":
         return 1+ count_vowels(s[1:])
-    return 1+ count_vowels(s[1:])
+    return 0 + count_vowels(s[1:])
 
 s= "Vowelcheck"
 print(count_vowels(s))
