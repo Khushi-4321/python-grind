4dcda08b22271240a80ba63bac191a5b0af17a8b
Khushi <kagarwal170106@gmail.com>
2026-04-03 23:53:11 +0530

    Day 24 - recursion: check palindrome

---
+++ Day24.py
@@ -0,0 +1,9 @@
+# Day 24 - Recursion: Check palindrome
+def is_palindrome(s):
+    if len(s) <= 1:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return is_palindrome(s[1:-1])
+
+print(is_palindrome("racecar"))
