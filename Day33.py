64b9f25a8b8d56d7c255f39c4c4ae89bf823cf49
Khushi <kagarwal170106@gmail.com>
2026-04-15 23:40:52 +0530

    Day33 problem

---
+++ Day33.py
@@ -0,0 +1,12 @@
+# LeetCode #4 — Reverse String (Easy). No hints. Brute force attempt first. 
+# Paste your code before asking anything.
+
+
+# You have a list of numbers:
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Using map and lambda, create a new list where:
+# - Even numbers are squared
+# - Odd numbers are cubed
+
+# Output: [1, 4, 27, 16, 125, 36, 343, 64, 729, 100]
