b719f5143bc4332f64a5b49aa39cee9624154817
Khushi <kagarwal170106@gmail.com>
2026-04-30 23:54:21 +0530

    Updated write file

--- Day38.py
+++ Day38.py
@@ -1,6 +1,7 @@
 with open("Day37.txt", "w") as f:
     f.write("Name: Khushi\n")
     f.write("Goal: Internship by May\n")
+    f.write("Academic focus required due to exams\n")
 
 with open("Day37.txt", "r") as f:
     print(f.read())
